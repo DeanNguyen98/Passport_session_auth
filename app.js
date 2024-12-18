07aa124a99ecca4cfd5beb58e76c762863595a04
DeanNguyen98 <nctuan.hn@gmail.com>
2024-12-19 00:06:11 +1100

    add session setup and routes setup
    add session setup and routes setup

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const Pool = require("pg");
-const express = require("express");
+const {Pool} = require("pg");
 const session = require("express-session");
 const pgSession = require("connect-pg-simple")(session);
 const passport = require("passport");
@@ -21,7 +20,7 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
 });
 
@@ -29,23 +28,51 @@ pool.connect((err) => {
     if (err) {
         console.error("Failed to connect to the database:", err);
     } else {
-        console.log("Connected to the PostgreSQL database");
+        console.log("Connected to the PostgreSQL database:", process.env.DB_DATABASE);
     }
 });
+
 
 /** 
  *  --------------SESSION SETUP
  */
 
+const sessionStore = new pgSession({
+    pool: pool,
+    tableName: "session",
+    createTableIfMissing: true,
+});
+
+
 app.use(
     session({
-        store: new pgSession({
-            pool,
-            tableName: "session",
-        }),
+        store: sessionStore,
         secret: process.env.SECRET,
         resave: false,
-        saveUninitialized: false,
+        saveUninitialized: true,
         cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
     }),
 );
+
+/** 
+ *  ------------ ROUTES ----------
+ */
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello, world </h1>")
+})
+
+app.get("/login", (req, res) => {
+    res.send("<h1>Login page</h1>")
+});
+
+app.post("/login", (req, res) => {});
+
+app.get("/register", (req, res) => {
+    res.send("<h1>Register page </h1>")
+});
+
+app.post("register", (req, res) => {})
+
+
+app.listen(3000);
